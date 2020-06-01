63c9d5049b3f22f27285c3a64906ac3960a97672
SofiaFE <sofia.forsberg.ehn@gmail.com>
2020-06-01 14:24:58 +0200

    Testar pga serverfix

--- EventVisitors_MVC/Controllers/RegistrationController.cs
+++ EventVisitors_MVC/Controllers/RegistrationController.cs
@@ -33,23 +33,15 @@ namespace EventVisitors_MVC.Controllers
             // REGISTRERING FUNKAR!
 
             registration.Profile_Role = "Besökare"; // Besökare blir standardroll för alla som registrerar sig
-            //RegistrationClass rc = new RegistrationClass();
-            //rc.Role = "Besökare";
             
             using (var client = new HttpClient())
             {
-                RegistrationClass Registration = new RegistrationClass { Email = registration.Profile_Email, Firstname = registration.Profile_Firstname, Lastname = registration.Profile_Lastname, Password = "123", Role = registration.Profile_Role };
+                // Har hårdkodat in lösenord vilket behövs läggas till i ProfileClass och sedan när vi skickar profileClass till vår REST och ska lagra i DB, undvik att lagra lösenordet
+                RegistrationClass Registration = new RegistrationClass { Email = registration.Profile_Email, Firstname = registration.Profile_Firstname, Lastname = registration.Profile_Lastname, Password = registration.Profile_Password, Role = registration.Profile_Role };
 
-                //ProfilesClass Registration = new ProfilesClass{ Profile_Email = registration.Profile_Email, Profile_Firstname = registration.Profile_Firstname, Profile_Lastname = registration.Profile_Lastname, Profile_Password=registration.Profile_Password, Profile_Role =registration.Profile_Role};
                 client.BaseAddress = new Uri("http://193.10.202.76/api/");
 
-                //HTTP POST
-                HttpResponseMessage postTask = await client.PostAsJsonAsync("visitor", Registration); //.Id;  Kolla med grupp1 att det är rätt metod. Kolla att det är rätt id, annars laboration.
-                // Kalla på Login-gruppens getMetod för att hämta deras lista Get string async
-
-                //postTask.Wait();
-                //var result = await postTask.Content.ReadAsStringAsync().Result; // var Response = Res.Content.ReadAsStringAsync().Result;
-
+                HttpResponseMessage postTask = await client.PostAsJsonAsync("visitor", Registration);
 
                 if (postTask.IsSuccessStatusCode)
                 {
@@ -57,13 +49,11 @@ namespace EventVisitors_MVC.Controllers
 
                     registration.Profile_User_Id = Int32.Parse(result.ToString());
 
-                    //SaveProfile k = new SaveProfile; //Kalla på metoden
                     ProfilesClass b = new ProfilesClass { Profile_Email = registration.Profile_Email, Profile_Firstname = registration.Profile_Firstname, Profile_Lastname = registration.Profile_Lastname, Profile_PhoneNr =registration.Profile_PhoneNr, Profile_Birthday = registration.Profile_Birthday, Profile_Role =registration.Profile_Role, Profile_User_Id=registration.Profile_User_Id};
                     SaveProfile(b);
                     return RedirectToAction("LoginUser", "Login");
                 }
-                //ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
-
+                
                 return View(registration);
             }
         }
@@ -72,7 +62,7 @@ namespace EventVisitors_MVC.Controllers
         {
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:19779/api/");
+                client.BaseAddress = new Uri("http://193.10.202.82//api/");
                 var postTask = client.PostAsJsonAsync("MyProfile", newProfile);
 
                 var result = postTask.Result;
